2a300f640cdba10dc295d856a8133381196b5c3c
Ingo Haschler <ih@ingohaschler.de>
2021-10-30 19:57:49 +0200

    Moved UI methods

--- src/pages/page-gyro.cpp
+++ src/pages/page-gyro.cpp
@@ -41,22 +41,50 @@ Action pageActions[] = {
 
 void PageGyro::draw(bool initialLoading) {
     MPU* mpu = hal->getMPU();
-    TFT* tft = hal->getTFT();
+    TFT_eSPI* tft = hal->getTFT()->getInternalTFT();
     if (initialLoading) {
-        // deactivateWifi();
-        tft->initDrawQuaternion();
+        tft->fillScreen(TFT_BLACK);
+        tft->setTextDatum(TL_DATUM);
+        tft->setTextColor(TFT_ORANGE, TFT_BLACK);
+        tft->drawString("q0 = ", 6, 10, 2);
+        tft->drawString("qx = ", 6, 30, 2);
+        tft->drawString("qy = ", 6, 50, 2);
+        tft->drawString("qz = ", 6, 70, 2);
+        tft->drawString("Y = ", 6, 90, 2);
+        tft->drawString("P = ", 6, 110, 2);
+        tft->drawString("R = ", 6, 130, 2);
         pmenu = -1;
     }
     if (millis() - timeTemperature > 300) {
-        mpu->updateMPU();
+        mpu->readMPU9250();
         if (pmenu >= 0) {
             //tft->drawBottomBar(getTimeout(), TFT_BLUE);
         } 
         else {
-            float q[7] = { 0, 0, 0, 0, 0, 0, 0 };
-            mpu->gagewatchRead(q);
-            mpu->getDMP(q);
-            tft->refreshDrawQuaternion(q);
+            //float q[7] = { 0, 0, 0, 0, 0, 0, 0 };
+            //mpu->gagewatchRead(q);
+            //mpu->getDMP(q);
+            //tft->refreshDrawQuaternion(q);
+            tft->setTextDatum(TL_DATUM);
+            tft->setTextColor(TFT_ORANGE, TFT_BLACK);
+            char q0[8] = "";
+            char qx[8] = "";
+            char qy[8] = "";
+            char qz[8] = "";
+            char syaw[8] = "";
+            char spitch[8] = "";
+            char sroll[8] = "";
+            sprintf(syaw, "%5.1f", mpu->getYaw());
+            sprintf(spitch, "%5.1f", mpu->getPitch());
+            sprintf(sroll, "%5.1f", mpu->getRoll());
+            tft->drawString(q0, 40, 10, 2);
+            tft->drawString(qx, 40, 30, 2);
+            tft->drawString(qy, 40, 50, 2);
+            tft->drawString(qz, 40, 70, 2);
+            tft->drawString(syaw, 40, 90, 2);
+            tft->drawString(spitch, 40, 110, 2);
+            tft->drawString(sroll, 40, 130, 2);
+
            char buf[100];
            sprintf(buf, "yaw: %2f, pitch: %2f, roll %2f", 
                         mpu->getYaw(), mpu->getPitch(), mpu->getRoll());
